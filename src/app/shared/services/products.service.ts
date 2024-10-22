62b2e315a7693c3c94f49180e878f8f12f82fcb9
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2024-10-22 17:54:08 -0300

    Refatoração da requisição da API

--- src/app/shared/services/products.service.ts
+++ src/app/shared/services/products.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { Product } from '../interfaces/product.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +9,6 @@ export class ProductsService {
   httpClient = inject(HttpClient);
 
   getAll(){
-    return this.httpClient.get<any>('/api/products')
+    return this.httpClient.get<Product[]>('/api/products')
   }
 }
