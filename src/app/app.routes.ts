0460ef0b5c4e96ff336a0167737d193faa8e4a07
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2024-10-24 18:48:54 -0300

    Volta para a pagina principal após inserir item

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,6 @@ export const routes: Routes = [{
 },
 {
     path: 'create-product',
-    loadComponent: () => import('./features/create/create.component').then(m => m.CreateComponent)
+    loadComponent: () => import('./features/create/create.component').then((m) => m.CreateComponent)
 }
 ];
--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -4,6 +4,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ProductsService } from '../../shared/services/products.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create',
@@ -13,20 +15,34 @@ import { ProductsService } from '../../shared/services/products.service';
   styleUrl: './create.component.css'
 })
 export class CreateComponent {
-  productService = inject(ProductsService)
+  productService = inject(ProductsService);
+
+  matSnackBar = inject(MatSnackBar);
+
+  Router = inject(Router);
 
   form = new FormGroup({
     title: new FormControl('', { nonNullable: true, validators: Validators.required })
   })
 
   onSubmit(){
-    this.productService.post({
-      title: this.form.controls.title.value
-    }).subscribe(() => {
-      alert("Sucesso");
-      this.form.reset()
-      
+    this.matSnackBar.open("Item criado", "Ok",{
+      duration: 1500,
+      horizontalPosition: 'right',
+      verticalPosition: 'top'
     });
+
+    this.Router.navigateByUrl('/').catch()
+    // this.productService.post({
+    //   title: this.form.controls.title.value
+    // }).subscribe(() => {
+    //   this.form.reset()
+      
+    // });
     
+  }
+
+  openSnackBar(){
+
   }
 }
