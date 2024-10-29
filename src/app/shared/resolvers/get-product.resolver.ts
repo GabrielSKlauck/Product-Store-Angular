353ec6c78947408517e85bf1fc4bdbe9f6c8b9f2
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2024-10-29 18:16:23 -0300

    Implementação de resolver

---
+++ src/app/shared/resolvers/get-product.resolver.ts
@@ -0,0 +1,9 @@
+import { inject } from "@angular/core"
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router"
+import { ProductsService } from "../services/products.service"
+
+export const getProduct = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+    const productService = inject(ProductsService)
+
+    return productService.get(route.paramMap.get('id') as string)
+}
